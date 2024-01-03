2ac8e38be314d9035b24483eeacf5459264de747
mapletoot <149334794+mapletoot@users.noreply.github.com>
2024-01-03 08:45:44 +0000

    Amended column names

--- jeopardy_data_analysis.py
+++ jeopardy_data_analysis.py
@@ -4,3 +4,16 @@ pd.set_option('display.max_columns', None)
 
 df = pd.read_csv("jeopardy.csv")
 print(df.head())
+
+# Check column names
+print(df.columns)
+
+# Stripping columns of white space and converting to camel case
+df.rename(columns = {"Show Number" : "show_id", " Air Date" : "date", " Round" : "round", " Category" : "category", " Value" : "value", " Question" : "question", " Answer" : "answer"}, inplace=True)
+print(df.columns)
+
+
+# queastion_searcher = lambda row: 
+
+
+
